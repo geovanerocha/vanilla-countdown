4393970dfa257614991d97229a915d95164cb4c7
Geovane Rocha <contato@geovanerocha.com>
2015-11-27 10:37:20 -0200

    identation and add custom end_text string

--- src/vanilla-countdown.js
+++ src/vanilla-countdown.js
@@ -11,14 +11,15 @@ var vanillaCountdown = function(options, element) {
 	}
 
 	var that = this,
-		YY = options.year,
-		MM = options.month,
-		DD = options.day,
-		HH = options.hour,
-		MI = options.minute,
-		SS = options.second;
+	      YY = options.year,
+	      MM = options.month,
+	      DD = options.day,
+	      HH = options.hour,
+	      MI = options.minute,
+	      SS = options.second;
 
-	options.text = options.text || true;
+	options.text		 = options.text || true;
+	options.end_text = options.end_text || 'Thank you for support us, the event has finished';
 
 	that.init = function() {
 		setInterval(function() {
@@ -31,33 +32,33 @@ var vanillaCountdown = function(options, element) {
 	};
 
 	function countDown() {
-		var actual_date   	 = new Date(),
-			actual_day 		 = actual_date.getDate(),
-	    	future_date 	 = new Date(YY,MM-1,DD,HH,MI,SS),
-	    	ss 	   		 	 = parseInt((future_date - actual_date) / 1000),
-	   		mm     			 = parseInt(ss / 60),
-	    	hh    			 = parseInt(mm / 60),
-	    	countdown_string = '',
-	    	dd     			 = parseInt(hh / 24);
+		var actual_date   		 = new Date(),
+				actual_day 	 			 = actual_date.getDate(),
+	    	future_date 			 = new Date(YY,MM-1,DD,HH,MI,SS),
+	    	ss    		 				 = parseInt((future_date - actual_date) / 1000),
+	    	mm     						 = parseInt(ss / 60),
+	    	hh    						 = parseInt(mm / 60),
+	    	countdown_string	 = '',
+	    	dd     						 = parseInt(hh / 24);
 
 	    ss = ss - (mm * 60);
 	    mm = mm - (hh * 60);
 	    hh = hh - (dd * 24);
 
 	    countdown_string = '<span class="vanilla-countdown-numbers"> \
-						    	<span class="number-day">{{number_day}}</span> \
-						    	<span class="number-hour">{{number_hour}}</span> \
-						    	<span class="number-minute">{{number_minute}}</span> \
-						    	<span class="number-second">{{number_second}}</span> \
-						   </span>';
+									            <span class="number-day">{{number_day}}</span> \
+									            <span class="number-hour">{{number_hour}}</span> \
+									            <span class="number-minute">{{number_minute}}</span> \
+									            <span class="number-second">{{number_second}}</span> \
+									        </span>';
 
 		if(!!options.text){
 			var text_template = '<span class="vanilla-countdown-texts">	\
-									<span class="text-day">dia</span> \
-							    	<span class="text-hour">{{text_hour}}</span> \
-							    	<span class="text-minute">{{text_minute}}</span> \
-							    	<span class="text-second">{{text_second}}</span> \
-							   </span>';
+														 <span class="text-day">dia</span> \
+													    	 <span class="text-hour">{{text_hour}}</span> \
+													    	 <span class="text-minute">{{text_minute}}</span> \
+													    	 <span class="text-second">{{text_second}}</span> \
+												   </span>';
 
 			countdown_string = countdown_string + text_template;
 		}
@@ -73,7 +74,8 @@ var vanillaCountdown = function(options, element) {
 			countdown_string = countdown_string.replace('{{text_second}}', (ss > 1) ? 'segundos' : 'segundo' );
 		}
 
-	    (options.day && options.day > actual_day) ? element.innerHTML = 'Thank you for support us, the event has finished' : element.innerHTML = countdown_string;
+	    (options.day && options.day > actual_day) ? element.innerHTML = options.end_text : element.innerHTML = countdown_string;
 
 	}
+	
 };
